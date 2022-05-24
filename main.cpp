c36ab0ad399a847803393139a3591231598ace68
xyttttt <1413570823@qq.com>
2022-05-25 04:29:39 +0800

    add co_env

--- main.cpp
+++ main.cpp
@@ -1,25 +1,41 @@
 #include <iostream>
-#include "co_manager.h"
+#include <pthread.h>
+#include <thread>
+#include "co_env.h"
 using namespace std;
 
+void co_func(co_thread& t,void* arg){
+    long v = (long)arg;
+    //printf("before v=%d\n",v);
+    t.yield_for(v*1000);
+    printf("v=%d\n",v);
+}
 
-void co_func1(){
+void co_func1(co_thread& t,void* arg){
     for(int i=0;i<10;++i){
-        cout<<"from co_func2:"<<i<<enld;
-        yield();
+        printf("in co_func1 i=%d\n",i);
+        t.yield_for(500);
     }
 }
-void co_func2(){
+void co_func2(co_thread& t,void* arg){
     for(int i=0;i<10;++i){
-        cout<<"from co_func2:"<<i<<enld;
-        yield();
+        printf("in co_func2 i=%d\n",i);
+        t.yield_for(500);
     }
 }
 
 int main(){
-    co_manager env;
-    env.add_task(co_func1);
-    env.add_task(co_func2);
-    env.run();
+    
+    co_env env;
+    env.add_task(co_func1,nullptr);
+    env.add_task(co_func2,nullptr);
+
+    env.loop();
+
+    for(int i=0;i<=20;++i){
+        env.add_task(co_func,(void*)i);
+    }
+    env.loop();
+    
     return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,21 +1,32 @@
 
 #include <iostream>
-#include "co_manager.h"
+#include "co_env.h"
 using namespace std;
 
-void func(co_thread& t){
-    for(int i=0;i<10;++i){
-        printf("%d\n",i);
+void func1(co_thread& t){
+    for(int i=1;i<10;i+=2){
+        cout<<"func1 i="<<i<<endl;
+        //printf("func1 i=%d\n",i);
+        t.yield();
+    }
+}
+
+void func2(co_thread& t){
+    for(int i=2;i<10;i+=2){
+        cout<<"func2 i="<<i<<endl;
+        //printf("func2 i=%d\n",i);
         t.yield();
     }
 }
 
 int main(){
-    co_thread t((void*)func);
+    co_thread t1((void*)func1);
+    co_thread t2((void*)func2);
     bool r = false;
     while(!r){
-        printf("r=%d\n",r);
-        r = t.run();
+        bool r1 = t1.run();
+        bool r2 = t2.run();
+        r = r1&r2;
     }
     return 0;
 }
